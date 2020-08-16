164fbdbdaea55825af113aa87233a50f27a2920a
Minettyx <30463302+Minettyx@users.noreply.github.com>
2020-08-16 19:42:48 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(function() {
     }
   })
 
-  $.getJSON('https://ccm.0kb.eu/api/latest', function(dataa) {
+  $.getJSON('https://ccm.0kb.eu/api/latestv2', function(dataa) {
     app.latest = dataa;
     app.ready = true;
   });
@@ -25,3 +25,56 @@ $(function() {
   $('.ui.icon.input').popup();
 
 });
+
+
+function timeSince(date) {
+
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    if(Math.floor(interval) == 1) {
+      return Math.floor(interval) + " anno";
+    } else {
+      return Math.floor(interval) + " anni";
+    }
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    if(Math.floor(interval) == 1) {
+      return Math.floor(interval) + " mese";
+    } else {
+      return Math.floor(interval) + " mesi";
+    }
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    if(Math.floor(interval) == 1) {
+      return Math.floor(interval) + " giorno";
+    } else {
+      return Math.floor(interval) + " giorni";
+    }
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    if(Math.floor(interval) == 1) {
+      return Math.floor(interval) + " ore";
+    } else {
+      return Math.floor(interval) + " ore";
+    }
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    if(Math.floor(interval) == 1) {
+      return Math.floor(interval) + " minuto";
+    } else {
+      return Math.floor(interval) + " minuti";
+    }
+  }
+  if(Math.floor(interval) == 1) {
+    return Math.floor(interval) + " secondo";
+  } else {
+    return Math.floor(interval) + " secondi";
+  }
+}
