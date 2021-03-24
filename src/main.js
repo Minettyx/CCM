27f404af0fbc1a51485e05fd1b48873786935137
Minettyx <git@minettyx.com>
2021-03-24 21:22:57 +0100

    removed fontawesome

--- src/main.js
+++ src/main.js
@@ -7,16 +7,16 @@ import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//import { library } from '@fortawesome/fontawesome-svg-core'
+//import { faHome } from '@fortawesome/free-solid-svg-icons'
+//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHome)
+//library.add(faHome)
 
 const app = createApp(App)
 
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
+//app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(VueAxios, axios)
 
 app.mount('#app')
