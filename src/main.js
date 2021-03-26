dcb3caded1ffcfcd736d01ab18e4ab1381121be6
Minettyx <git@minettyx.com>
2021-03-26 13:03:48 +0100

    error toaster

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Toaster from '@meforma/vue-toaster';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
@@ -18,5 +19,6 @@ const app = createApp(App)
 app.use(router)
 //app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(VueAxios, axios)
+app.use(Toaster)
 
 app.mount('#app')
