3739ad0dfae04ea7450bc1a286515a6f825b0182
Minettyx <30463302+Minettyx@users.noreply.github.com>
2021-06-03 10:21:09 +0200

    reading order

--- src/views/Chapter.vue
+++ src/views/Chapter.vue
@@ -13,14 +13,14 @@
     <div v-if="!loading" class="container" :style="{'opacity': gui ? '1' : '0', 'visibility': gui ? 'visible' : 'hidden'}" style="display: flex; align-items: center; text-align: left; transition: visibility 0.25s, opacity 0.25s linear;">
       <div style="padding: 6px 10px; border-radius: 100px;" class="cborder"><router-link :to="'/manga/'+this.$route.params.manga"><fai icon='arrow-left' class="fa-lg"/></router-link></div>
       <div style="padding: 7px 7px 2px 7px;" class="cborder"><h6>{{ manga.title }}</h6><small>{{ (data.volume ? 'Vol.'+data.volume+' ' : '') + 'Ch.'+data.chapter }}{{ data.title ? ' - '+data.title : '' }}</small></div>
-      <!-- <div style="padding: 6px 10px; border-radius: 100px; margin-left: auto;" class="cborder"><a type="button"><fai icon='cog' class="fa-lg"/></a></div> -->
+      <div style="padding: 6px 10px; border-radius: 100px; margin-left: auto;" class="cborder"><a type="button" data-bs-toggle="modal" data-bs-target="#settingsmodal"><fai icon='cog' class="fa-lg"/></a></div>
     </div>
   </div>
 
   <div style="position: fixed; width: 100%; left: 0; right: 0; bottom: 10px;">
     <div v-if="!loading" class="container" :style="{'opacity': gui ? '1' : '0', 'visibility': gui ? 'visible' : 'hidden'}" style="display: flex; align-items: center; transition: visibility 0.25s, opacity 0.25s linear;">
       <router-link v-if="this.$route.params.id!=manga.chapters[0].chapter" :to="'/chapter/'+this.$route.params.manga+'/'+getChapter(-1)" class="cborder" style="padding: 1px 8px; margin-right: auto;"><fai icon='chevron-left' class="fa-lg"/></router-link>
-      <input v-if="!data.webtoon" type="range" class="form-range cborder" min="0" :max="data.images.length-1" step="0" v-model="readingpage" style="padding: 10px;">
+      <input v-if="!data.webtoon" type="range" class="form-range cborder" min="0" :max="data.images.length-1" step="0" v-model="readingpage" style="padding: 10px;" :style="{ 'direction': readdir }">
       <router-link v-if="this.$route.params.id!=manga.chapters.slice().pop().chapter" :to="'/chapter/'+this.$route.params.manga+'/'+getChapter(1)" class="cborder" style="padding: 1px 8px; margin-left: auto;"><fai icon='chevron-right' class="fa-lg"/></router-link>
     </div>
   </div>
@@ -51,6 +51,27 @@
     <div class="col-0 col-lg-1 col-xl-2"></div>
   </div>
 
+  <!-- Modal -->
+  <div class="modal fade" id="settingsmodal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Impostazioni</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>Direzione di lettura</p>
+          <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="btnradio" value="ltr" id="btnradio1" autocomplete="off" v-model="readdir" checked>
+            <label class="btn btn-outline-primary" for="btnradio1">Da sinistra a destra</label>
+            <input type="radio" class="btn-check" name="btnradio" value="rtl" id="btnradio2" autocomplete="off" v-model="readdir">
+            <label class="btn btn-outline-primary" for="btnradio2">Da destra a sinistra</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 </template>
 
@@ -68,7 +89,8 @@ export default defineComponent({
       loadlimit: 0,
       loadedimages: [],
       firsttoload: 0,
-      gui: true
+      gui: true,
+      readdir: 'ltr'
     }
   },
   mounted() {
@@ -82,6 +104,7 @@ export default defineComponent({
         }
       }
     }
+    this.readdir = this.getCookie('readdir')
   },
   beforeUnmount() {
     window.onkeydown = () => {return}
@@ -98,6 +121,11 @@ export default defineComponent({
     'readingpage': {
       handler() {
         history.pushState(history.state, '', '#'+(parseInt(this.readingpage)+1))
+      }
+    },
+    'readdir': {
+      handler() {
+        this.setCookie('readdir', this.readdir, 36500)
       }
     }
   },
@@ -145,17 +173,35 @@ export default defineComponent({
       return res
     },
     readnext() {
+      if(this.readdir == 'rtl') {
+        this.realprevious()
+      } else {
+        this.realnext()
+      }
+    },
+    readprevious() {
+      if(this.readdir == 'rtl') {
+        this.realnext()
+      } else {
+        this.realprevious()
+      }
+    },
+    realnext() {
       if(this.readingpage != this.data.images.length-1) {
         this.readingpage++
       } else if(this.$route.params.id!=this.manga.chapters.slice().pop().chapter) {
         this.$router.push('/chapter/'+this.$route.params.manga+'/'+this.getChapter(1))
+      } else {
+        this.$router.push('/manga/'+this.$route.params.manga)
       }
     },
-    readprevious() {
+    realprevious() {
       if(this.readingpage != 0) {
         this.readingpage--
       } else if(this.$route.params.id!=this.manga.chapters[0].chapter) {
         this.$router.push('/chapter/'+this.$route.params.manga+'/'+this.getChapter(-1)+'#last')
+      } else {
+        this.$router.push('/manga/'+this.$route.params.manga)
       }
     },
     updatefirsttoload() {
@@ -179,6 +225,26 @@ export default defineComponent({
       } else {
         this.gui = !this.gui
       }
+    },
+    /* https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript */
+    setCookie(name, value, days = false) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    },
+    getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
     }
   }
 })
